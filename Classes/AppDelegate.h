4830195d2fd28d6c84a24c4ca7aeea463e9a7a27
Travis Spangle <tspangle@Travis-Spangles-MacBook.local>
2011-01-28 09:50:13 -0800

    Initial project created.

---
+++ Classes/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  WeatherTrackerAppDelegate.h
+//  WeatherTracker
+//
+//  Created by Travis Spangle on 1/28/11.
+//  Copyright 2011 Peak Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WeatherTracker
+//
+//  Created by Travis Spangle on 1/28/11.
+//  Copyright 2011 Peak Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
